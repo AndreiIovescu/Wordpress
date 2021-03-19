4047d90dd68616b697a9d1afb8a52818e4113c31
Andrei_Iovescu <andreiiovescu1@gmail.com>
2021-03-19 20:23:30 +0200

    Added component parameter

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ offers = []
 assignment_matrix = []
 vm_types = []
 conflicts = {}
+added_component = []
 
 
 def get_component_requirements():
     with open('data.txt') as f:
         components_list = json.load(f)
-        return components_list["Assignment Matrix"]
+        return components_list["Components Requirements"]
 
 
 def get_offers():
@@ -37,14 +38,21 @@ def get_conflicts():
         return components_list["Conflicts"]
 
 
+def get_added_component():
+    with open('data.txt') as f:
+        components_list = json.load(f)
+        added_component_id = components_list["Added Component"]
+        return component_requirements[added_component_id]
+
+
 # the function first checks whether the two components could be in conflict
 # if they can't, it will return false as there is no conflict and we don't have to check
 # if the two can be in conflict, then we have to check the assignment matrix
 # if the two rows that correspond to the components have the value 1 at the same position, then there exists a conflict
 # that is because they have been deployed on the same machine although that does not follow the constraints
-def check_conflict(assign_matrix, conf, component_id, component2_id):
-    for component in conf:
-        if int(component) == component_id and component2_id in conf[component]:
+def check_conflict(assign_matrix, component_id, component2_id):
+    for component in conflicts:
+        if int(component) == component_id and component2_id in conflicts[component]:
             for i in range(len(assign_matrix[0])):
                 if assign_matrix[component_id][i] == assign_matrix[component2_id][i] == 1:
                     return True
@@ -70,4 +78,8 @@ if __name__ == '__main__':
     conflicts = get_conflicts()
     print(conflicts)
 
-    print(check_conflict(assignment_matrix, conflicts, 4, 1))
+    print(check_conflict(assignment_matrix, 4, 1))
+
+    added_component = get_added_component()
+    print(added_component)
+
