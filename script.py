a2cdc2045380068398a6a0e58f7ab7a0332693f4
Andrei_Iovescu <andreiiovescu1@gmail.com>
2021-05-21 11:05:18 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,9 @@
 from minizinc import Instance, Model, Solver
+from datetime import timedelta
 import csv
+import time
+import os
+import json
 
 
 def get_min_machine_number(file, component_number):
@@ -19,22 +23,53 @@ def solve_model_minizinc(model_path, problem_instances_number, solver, offers_nu
     # Create an instance of the problem using the previous solver
     instance = Instance(solver, model)
     # Links the corresponding dzn file to the model
-    instance.add_file(f"{model_path.replace('.mzn','')}_Offers{offers_number}.dzn")
+    instance.add_file(f"{model_path.replace('.mzn', '')}_Offers{offers_number}.dzn")
     # Assign the number of wordpress instances and the minimum machines number
-    instance["M"] = get_min_machine_number(f"{model_path.replace('.mzn','')}_Surrogate.csv", problem_instances_number)
+    instance["M"] = get_min_machine_number(f"{model_path.replace('.mzn', '')}_Surrogate.csv", problem_instances_number)
     instance["WP"] = problem_instances_number
-    result = instance.solve()
-    return result
+    start_time = time.time()
+    result = instance.solve(timeout=timedelta(milliseconds=2400000))
+    run_time = time.time() - start_time
+    return result, run_time
+
+
+def write_output(model_file, component_number, offer_number, price_array, run_time):
+    directory = 'MiniZinc_Output'
+    file = f"MiniZinc_Output\\{model_file.replace('.mzn', '')}{component_number}_Offers{offer_number}_MiniZinc.csv"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    with open(file, mode='w', newline='') as f:
+        fieldnames = ['Price min value', 'Price for each machine', 'Time']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+        writer.writeheader()
+        min_price = sum(price_array)
+        writer.writerow({'Price min value': min_price, 'Price for each machine': price_array, 'Time': run_time})
+
+
+def create_greedy_input(model_file, component_number, offer_number, assignment_matrix, price_array, type_array):
+    directory = 'Greedy_Input'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    file = f"Greedy_Input\\{model_file.replace('.mzn', '')}{component_number}_Offers{offer_number}_Input.json"
+    data = {
+        "Assignment Matrix": assignment_matrix,
+        "Price Array": price_array,
+        "Type Array": type_array
+    }
+    with open(file, 'w') as f:
+        json.dump(data, f)
 
 
 if __name__ == '__main__':
-    model_file = "Wordpress.mzn"
+    output, runtime = solve_model_minizinc("Wordpress.mzn", 3, "chuffed", 20)
+    write_output("Wordpress.mzn", 3, 20, output['price'], runtime)
+    create_greedy_input("Wordpress.mzn", 3, 20, output['a'], output['price'],  output['t'])
+    """model_file = "Wordpress.mzn"
     Solvers = ["chuffed", "gecode", "or-tools"]
     offers_numbers = [20, 40, 250, 500]
     for solver in Solvers:
         for component_instances in range(3, 13):
             for number in offers_numbers:
                 output = solve_model_minizinc(model_file, component_instances, solver, number)
-                print(output)
-
-
+                print(output)"""
