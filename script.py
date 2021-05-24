556292bc28c9dfffe9b5aa8abf0fcea0bc8883fe
Andrei_Iovescu <andreiiovescu1@gmail.com>
2021-05-24 13:41:03 +0300

    Update script.py

--- script.py
+++ script.py
@@ -6,10 +6,16 @@ import os
 import json
 
 
+# This function returns the mapping between the number of components deployed and the max machine number
+# For ex: 3 wordpress -> max 8 machines, 4 wordpress -> max 10 machines, in our case 3 and 4 are "component_number"
 def get_min_machine_number(file, component_number):
     with open(file, 'r') as f:
         content = csv.DictReader(f)
+        # Since the content of the csv file depends on what problem we solve, we want to extract the problem name
+        # In this convention, we do that by removing the _surrogate.csv part and also the directory header
         application_name = file.replace('_Surrogate.csv', '').lower()
+        application_name = application_name.replace('surrogate\\', '')
+        # We go through the csv file and if we find the number received as parameter we can return it's mapping
         for row in content:
             if row[f'{application_name}_instances'] == str(component_number):
                 return int(row['vm_number'])
@@ -23,12 +29,12 @@ def solve_model_minizinc(model_path, problem_instances_number, solver, offers_nu
     # Create an instance of the problem using the previous solver
     instance = Instance(solver, model)
     # Links the corresponding dzn file to the model
-    directory = 'DZN_Files'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    instance.add_file(f"{directory}\\{model_path.replace('.mzn', '')}_Offers{offers_number}.dzn")
+    instance.add_file(f"DZN_Files\\{model_path.replace('.mzn', '')}_Offers{offers_number}.dzn")
     # Assign the number of wordpress instances and the minimum machines number
-    instance["M"] = get_min_machine_number(f"{model_path.replace('.mzn', '')}_Surrogate.csv", problem_instances_number)
+    instance["M"] = get_min_machine_number(
+        f"Surrogate\\{model_path.replace('.mzn', '')}_Surrogate.csv",
+        problem_instances_number
+    )
     instance["WP"] = problem_instances_number
     start_time = time.time()
     result = instance.solve(timeout=timedelta(milliseconds=2400000))
@@ -37,10 +43,8 @@ def solve_model_minizinc(model_path, problem_instances_number, solver, offers_nu
 
 
 def write_output(model_file, component_number, offer_number, price_array, run_time):
-    directory = 'MiniZinc_Output'
-    file = f"{directory}\\{model_file.replace('.mzn', '')}{component_number}_Offers{offer_number}_MiniZinc.csv"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    create_directory("MiniZinc_Output")
+    file = f"MiniZinc_Output\\{model_file.replace('.mzn', '')}{component_number}_Offers{offer_number}_MiniZinc.csv"
     with open(file, mode='w', newline='') as f:
         fieldnames = ['Price min value', 'Price for each machine', 'Time']
         writer = csv.DictWriter(f, fieldnames=fieldnames)
@@ -51,10 +55,8 @@ def write_output(model_file, component_number, offer_number, price_array, run_ti
 
 
 def create_greedy_input(model_file, component_number, offer_number, assignment_matrix, price_array, type_array):
-    directory = 'Greedy_Input'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    file = f"{directory}\\{model_file.replace('.mzn', '')}{component_number}_Offers{offer_number}_Input.json"
+    create_directory("Greedy_Input")
+    file = f"Greedy_Input\\{model_file.replace('.mzn', '')}{component_number}_Offers{offer_number}_Input.json"
     data = {
         "Assignment Matrix": assignment_matrix,
         "Price Array": price_array,
